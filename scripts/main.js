caac4b7695a41ec8a33a4835dad7b203147eaa97
Idle <651462+reonZ@users.noreply.github.com>
2022-11-18 17:43:05 +0100

    option to remove efffects and added settings

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,39 @@
-import { onPF2eEffectClick, onPF2eEffectRightClick } from './pf2e.js'
-import { localize } from './utils/foundry.js'
+import { addInstructions, setEvents } from './effect.js'
+import { registerSetting } from './utils/foundry.js'
 
-/**
- * @param {JQuery.ClickEvent | JQuery.ContextMenuEvent} event
- * @param {EffectsPanel} panel
- */
-function getEffect(event, panel) {
-    const target = /** @type {HTMLElement} */ (event.currentTarget)
-    const id = /** @type {string} */ (target.dataset.itemId)
-    return panel.actor?.items.get(id)
+function refreshEffectsPanel() {
+    const g = /** @type {GamePF2e} */ (game)
+    g.pf2e.effectPanel?.render()
 }
 
-/** @param {JQuery} $html */
-function addInstructions($html) {
-    const instructions = `<p>${localize('sheet')}</p><p>${localize('chat')}</p>`
-    $html.find('.instructions').append(instructions)
-}
+Hooks.once('init', () => {
+    registerSetting({
+        name: 'remove',
+        type: Boolean,
+        default: true,
+        scope: 'client',
+        config: true,
+        onChange: refreshEffectsPanel,
+    })
 
-/**
- * @param {EffectsPanel} panel
- * @param {JQuery} $html
- */
-function setEvents(panel, $html) {
-    const $icons = $html.find('div[data-item-id]')
-    const clickFn = /** @param {JQuery.ClickEvent} event */ event => onEffectClick(event, panel)
-    const rightClickFn = /** @param {JQuery.ContextMenuEvent} event */ event => onEffectRightClick(event, panel)
+    registerSetting({
+        name: 'sheet',
+        type: Boolean,
+        default: true,
+        scope: 'client',
+        config: true,
+        onChange: refreshEffectsPanel,
+    })
 
-    $icons.off('click contextmenu')
-    $icons.on('click', clickFn)
-    $icons.on('contextmenu', rightClickFn)
-}
-
-/**
- * @param {JQuery.ClickEvent} event
- * @param {EffectsPanel} panel
- */
-async function onEffectClick(event, panel) {
-    if (event.ctrlKey) getEffect(event, panel)?.sheet?.render(true)
-    else onPF2eEffectClick(event, panel)
-}
-
-/**
- * @param {JQuery.ContextMenuEvent} event
- * @param {EffectsPanel} panel
- */
-function onEffectRightClick(event, panel) {
-    if (event.ctrlKey) getEffect(event, panel)?.toChat()
-    else onPF2eEffectRightClick(event, panel)
-}
+    registerSetting({
+        name: 'chat',
+        type: Boolean,
+        default: true,
+        scope: 'client',
+        config: true,
+        onChange: refreshEffectsPanel,
+    })
+})
 
 Hooks.on('renderEffectsPanel', (/** @type {EffectsPanel} */ panel, /** @type {JQuery} */ $html) => {
     addInstructions($html)
--- scripts/utils/foundry.js
+++ scripts/utils/foundry.js
@@ -9,3 +9,31 @@ export function localize(key, data) {
     if (data) return game.i18n.format(key, data)
     return game.i18n.localize(key)
 }
+
+/** @param {string[]} path */
+function getSettingLocalizationPath(...path) {
+    return `${MODULE_ID}.settings.${path.join('.')}`
+}
+
+/**
+ * @template {*} T
+ * @param {Omit<RequiredBy<SettingConfig<T>, 'name'>, 'key' | 'namespace'>} options
+ * options.scope = 'world'
+ *
+ * options.config = false
+ */
+export function registerSetting(options) {
+    const name = options.name
+    options.scope = options.scope ?? 'world'
+    options.config = options.config ?? false
+    if (options.config) {
+        options.name = getSettingLocalizationPath(name, 'name')
+        options.hint = getSettingLocalizationPath(name, 'hint')
+    }
+    game.settings.register(MODULE_ID, name, options)
+}
+
+/**@param {string} key*/
+export function getSetting(key) {
+    return game.settings.get(MODULE_ID, key)
+}
